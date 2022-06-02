4b951778286ba193d53b8950eb22028faccefa85
godud2604 <ees238@naver.com>
2022-06-02 09:26:17 +0900

    Setting deployment, production environments

---
+++ webpack.dev.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './dist',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+}
---
+++ webpack.prod.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: './dist',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: ['file-loader'],
+      },
+    ],
+  },
+}
